a36c60ee08431a7d1b9c7a2bcd273e97c40d36bf
zaelynnperez <72786696+zaelynnperez@users.noreply.github.com>
2021-01-21 17:25:19 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
     music.set_volume(70)
-    if input.sound_level() > 10 and input.light_level() <= 5:
+    if input.sound_level() <= 10 and input.light_level() <= 5:
         for i in range(5):
             music.ba_ding.play_until_done()
     music .stop_all_sounds()
