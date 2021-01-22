6fa5ab0450a431609cc76f60b80421da0c822f34
zaelynnperez <72786696+zaelynnperez@users.noreply.github.com>
2021-01-21 20:02:32 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,12 +1,7 @@
 while (true) {
     music.setVolume(70)
-    if (input.soundLevel() <= 10 && input.lightLevel() <= 5) {
-        for (let i = 0; i < 5; i++) {
-            music.baDing.playUntilDone()
-        }
-        light.setAll(light.rgb(255, 255, 255))
+    if (input.soundLevel() >= 8 && input.lightLevel() <= 5) {
+        music.baDing.playUntilDone()
     }
     
-    music.stopAllSounds()
-    pause(10000)
 }
